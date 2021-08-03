e18f4a6500f88f5415ddb1dd7a256a17a5e90a0f
Majdi-10 <majdi10_joiblia10@hotmail.com>
2021-08-03 02:24:28 +0100

    starting the application

---
+++ MajdiUnnsplash/database/index.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+const createTables = require('./config');
+const Promise = require('bluebird');
+const database = 'Unsplash';
+
+const connection = mysql.createConnection({
+  user: 'Majdi_10',
+  password: 'Majdi_joiblia_10'
+});
+
+const db = Promise.promisifyAll(connection, { multiArgs: true });
+
+db.connectAsync()
+  .then(() => console.log(`Connected to ${database} database as ID ${db.threadId}`))
+  .then(() => db.queryAsync(`CREATE DATABASE IF NOT EXISTS ${database}`))
+  .then(() => db.queryAsync(`USE ${database}`))
+  .then(() => createTables(db));
+
+module.exports = db;
